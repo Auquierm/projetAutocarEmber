96dd52dfae645a46165cfb7e485c5cee82519347
Auquier Maxime <stonne610@hotmail.com>
2019-02-28 14:24:41 +0100

    modified routes proposal

--- app-autocar/app/helpers/multiply.js
+++
@@ -1,9 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function multiply(params/*, hash*/) {
-  return params.reduce((a, b) => {
-    return a * b;
-  });
-}
-
-export default helper(multiply);
