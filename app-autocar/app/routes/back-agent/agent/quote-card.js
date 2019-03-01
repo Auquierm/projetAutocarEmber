db8509319e2ed9fc6872b96a879f95c302c47fde
aurore aurore <aurore.lietard@gmail.com>
2019-03-01 15:50:54 +0100

    back-agent

--- app-autocar/app/routes/back-agent/agent/quote-card.js
+++ app-autocar/app/routes/back-agent/agent/quote-card.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route'
 
 export default Route.extend({
   model(params){
-return this.store.findRecord('quote', params.idQuote)
+return this.store.findRecord('quote', params.idquote)
   },
   actions: {
     didTransition() {
