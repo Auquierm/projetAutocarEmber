9e3072c09ae9bfb22a230f9ae5e1cf1a03201a02
Auquier Maxime <stonne610@hotmail.com>
2019-03-04 15:17:55 +0100

    merging and working on the price calculation

--- api-autocar/api/routes/v1/pricing.route.js
+++ api-autocar/api/routes/v1/pricing.route.js
@@ -2,23 +2,21 @@ const Express = require('express');
 const PricingController = require(`${process.cwd()}/api/controllers/pricing.controller`);
 const Validate = require('express-validator');
 
-const { listPricings, createPricing, updatePricing} = require('./../../validations/pricing.validation');
+const { listPricings, createPricing, updatePricing } = require('./../../validations/pricing.validation');
 
-const {authorize, ADMIN} = require('./../../middlewares/auth.middleware');
+const { authorize, ADMIN } = require('./../../middlewares/auth.middleware');
 
 const router = Express.Router();
 
 router
     .route('/')
-        .get(
-            // authorize(ADMIN), 
-            Validate(listPricings), PricingController.findAll)
-        .post(/*authorize(ADMIN), */Validate(createPricing), PricingController.add);
+    .get(
+        // authorize(ADMIN), 
+        Validate(listPricings), PricingController.findAll)
+    .post(/*authorize(ADMIN), */Validate(createPricing), PricingController.add);
 
 router
     .route('/:pricingId')
-        .patch(
-            // authorize(ADMIN), 
-            Validate(updatePricing), PricingController.update);
+    .patch(/*authorize(ADMIN),*/ Validate(updatePricing), PricingController.update);
 
 module.exports = router;
--- app-autocar/app/routes/back-agent/agent/quote-card.js
+++ app-autocar/app/routes/back-agent/agent/quote-card.js
@@ -1,8 +1,16 @@
-import Route from '@ember/routing/route'
+// import Route from '@ember/routing/route'
 
-export default Route.extend({
+export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('quote', params.idquote)
+    // return this.store.findRecord('quote', params.idquote)
+    return Ember.RSVP.hash({
+      quote: this.store.findRecord('quote', params.idquote),
+      pricing: this.store.findAll('pricing')
+    })
+  },
+  setupController(controller, models) {
+    controller.set('quote', models.quote);
+    controller.set('pricing', models.pricing);
   },
   actions: {
     didTransition() {
