a11ff4f99a5728da75801886dca756c7c57a86a6
Auquier Maxime <stonne610@hotmail.com>
2019-03-05 16:34:17 +0100

    modified folder-card

--- app-autocar/app/routes/back-agent/agent/folder-card.js
+++ app-autocar/app/routes/back-agent/agent/folder-card.js
@@ -1,8 +1,22 @@
-import Route from '@ember/routing/route';
+// import Route from '@ember/routing/route'
+import {hash} from 'rsvp';
 
-export default Route.extend({
-  model(params) {
-    return this.store.findRecord('quote', params.idfolder)
+export default Ember.Route.extend({
+  async model(params) {
+    return hash({
+      quote: await this.store.findRecord('quote', params.idfolder),
+      pricing: await this.store.findAll('pricing')
+    })
+  },
+  setupController(controller, models) {
+    controller.set('quote', models.quote);
+    controller.set('pricing', models.pricing);
+  },
+  resetController(controller){
+    controller.setProperties({
+      "totalKm":"",
+      "addCost":""
+    })
   },
   actions: {
     didTransition() {
