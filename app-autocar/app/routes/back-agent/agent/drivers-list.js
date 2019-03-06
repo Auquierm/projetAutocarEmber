42fd85f8fab9526e6f187ded009f4b71bbe74ad3
Sacha <sachau888@hotmail.com>
2019-03-06 15:00:32 +0100

    create-agent and start bug killing

--- app-autocar/app/routes/back-agent/agent/drivers-list.js
+++ app-autocar/app/routes/back-agent/agent/drivers-list.js
@@ -2,10 +2,8 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(){
-     return this.store.findAll('driver')
-
-      },
-
+    return this.store.findAll('driver')
+  },
   actions: {
     didTransition() {
       document.title = 'Agent - Liste chauffeurs';
