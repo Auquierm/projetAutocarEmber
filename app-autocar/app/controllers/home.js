c2ef8fba4087be0e37bba273af356d72173d7b21
Auquier Maxime <stonne610@hotmail.com>
2019-03-08 08:47:35 +0100

    sponsoring nostélé

--- app-autocar/app/controllers/home.js
+++ app-autocar/app/controllers/home.js
@@ -1,5 +1,4 @@
 import Controller from '@ember/controller';
-// import Contact from '../models/contact';
 let larg = window.innerWidth;
 
 export default Controller.extend({
@@ -11,11 +10,6 @@ export default Controller.extend({
     },
     sendMessage(e) {
       e.preventDefault();
-      // console.log('coucou');
-      // console.log(this.get('contactFirstname'));
-      // let newMessage = Contact.create({firstname:this.get('contactFirstname')});
-      // // this.model.pushObject(newMessage);
-      // console.log(newMessage);
     }
   }
 });
